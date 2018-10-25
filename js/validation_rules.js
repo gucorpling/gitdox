82e813ece917a19fbf33103f7cf2e39bd1a281b9
Luke Gessler <lukegessler@gmail.com>
2018-10-25 01:42:02 -0400

    extend validation rules creation page for xml and export validation

--- js/validation_rules.js
+++ js/validation_rules.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
-    $('#ValidationTableContainer').jtable({
-        title: 'Validation Rules',
+    $('#xml-table-container').jtable({
+        title: 'XML Validation Rules',
         sorting: true,
         actions: {
-            listAction: 'modules/jtable_rule_list.py',
+            listAction: function (postData, jtParams) {
+                return $.Deferred(function ($dfd) {
+                    $.ajax({
+                        url: 'modules/jtable_rule_list.py',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: {domain: "xml"},
+                        success: function (data) {
+                            $dfd.resolve(data);
+                        },
+                        error: function() {
+                            $dfd.reject();
+                        }
+                    });
+                });
+            },
             createAction: 'modules/jtable_create_rule.py',
             updateAction: 'modules/jtable_update_rule.py',
             deleteAction: 'modules/jtable_delete_rule.py'
@@ -11,7 +26,13 @@ $(document).ready(function () {
         fields: {
             id: {
                 title: 'ID',
-                key: true
+                key: true,
+                visibility:'hidden'
+            },
+            domain: {
+                title: 'Domain',
+                options: ['xml'],
+                visibility: 'hidden'
             },
             doc: {
                 title: 'Document'
@@ -19,9 +40,113 @@ $(document).ready(function () {
             corpus: {
                 title: 'Corpus'
             },
+            name: {
+                title: 'XSD Schema',
+                options: 'modules/jtable_schema_list.py?extension=xsd'
+            }
+        }
+    });
+    $('#xml-table-container').jtable('load');
+});
+
+$(document).ready(function () {
+    $('#meta-table-container').jtable({
+        title: 'Metadata Validation Rules',
+        sorting: true,
+        actions: {
+            listAction: function (postData, jtParams) {
+                return $.Deferred(function ($dfd) {
+                    $.ajax({
+                        url: 'modules/jtable_rule_list.py',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: {domain: "meta"},
+                        success: function (data) {
+                            $dfd.resolve(data);
+                        },
+                        error: function() {
+                            $dfd.reject();
+                        }
+                    });
+                });
+            },
+            createAction: 'modules/jtable_create_rule.py',
+            updateAction: 'modules/jtable_update_rule.py',
+            deleteAction: 'modules/jtable_delete_rule.py'
+        },
+        fields: {
+            id: {
+                title: 'ID',
+                key: true,
+                visibility:'hidden'
+            },
             domain: {
                 title: 'Domain',
-                options: ['ether', 'meta']
+                options: ['meta'],
+                visibility: 'hidden'
+            },
+            doc: {
+                title: 'Document'
+            },
+            corpus: {
+                title: 'Corpus'
+            },
+            name: {
+                title: 'Name'
+            },
+            operator: {
+                title: 'Operator',
+                options: ['~', 'exists']
+            },
+            argument: {
+                title: 'Argument'
+            }
+        }
+    });
+    $('#meta-table-container').jtable('load');
+});
+
+$(document).ready(function () {
+    $('#ether-table-container').jtable({
+        title: 'EtherCalc Validation Rules',
+        sorting: true,
+        actions: {
+            listAction: function (postData, jtParams) {
+                return $.Deferred(function ($dfd) {
+                    $.ajax({
+                        url: 'modules/jtable_rule_list.py',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: {domain: "ether"},
+                        success: function (data) {
+                            $dfd.resolve(data);
+                        },
+                        error: function() {
+                            $dfd.reject();
+                        }
+                    });
+                });
+            },
+            createAction: 'modules/jtable_create_rule.py',
+            updateAction: 'modules/jtable_update_rule.py',
+            deleteAction: 'modules/jtable_delete_rule.py'
+        },
+        fields: {
+            id: {
+                title: 'ID',
+                key: true,
+                visibility:'hidden'
+            },
+            domain: {
+                title: 'Domain',
+                options: ['ether'],
+                visibility: 'hidden'
+            },
+            doc: {
+                title: 'Document'
+            },
+            corpus: {
+                title: 'Corpus'
             },
             name: {
                 title: 'Name'
@@ -35,5 +160,60 @@ $(document).ready(function () {
             }
         }
     });
-    $('#ValidationTableContainer').jtable('load');
+    $('#ether-table-container').jtable('load');
+});
+
+$(document).ready(function () {
+    $('#export-table-container').jtable({
+        title: 'Export Validation Rules',
+        sorting: true,
+        actions: {
+            listAction: function (postData, jtParams) {
+                return $.Deferred(function ($dfd) {
+                    $.ajax({
+                        url: 'modules/jtable_rule_list.py',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: {domain: "export"},
+                        success: function (data) {
+                            $dfd.resolve(data);
+                        },
+                        error: function() {
+                            $dfd.reject();
+                        }
+                    });
+                });
+            },
+            createAction: 'modules/jtable_create_rule.py',
+            updateAction: 'modules/jtable_update_rule.py',
+            deleteAction: 'modules/jtable_delete_rule.py'
+        },
+        fields: {
+            id: {
+                title: 'ID',
+                key: true,
+                visibility:'hidden'
+            },
+            domain: {
+                title: 'Domain',
+                options: ['export'],
+                visibility: 'hidden'
+            },
+            doc: {
+                title: 'Document'
+            },
+            corpus: {
+                title: 'Corpus'
+            },
+            name: {
+                title: 'Export Spec',
+                options: 'modules/jtable_schema_list.py?extension=ini'
+            },
+            argument: {
+                title: 'XSD Schema',
+                options: 'modules/jtable_schema_list.py?extension=xsd'
+            }
+        }
+    });
+    $('#export-table-container').jtable('load');
 });
