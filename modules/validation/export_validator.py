0f47e90c1d5f35b7e6340276d8aa152bcd6312f2
amir-zeldes <amir.zeldes@georgetown.edu>
2018-10-28 19:51:34 -0400

    HTML escaping and whitespace   * Some XML validation messages could mess up HTML rendering of report

--- modules/validation/export_validator.py
+++ modules/validation/export_validator.py
@@ -4,36 +4,37 @@ import re
 
 # TODO: would have been ideal to write this without any filesystem operations
 class ExportValidator(Validator):
-    def __init__(self, rule):
-        self.corpus = rule[0]
-        self.doc = rule[1]
-        self.config = rule[3]
-        self.schema = rule[5]
+	def __init__(self, rule):
+		self.corpus = rule[0]
+		self.doc = rule[1]
+		self.config = rule[3]
+		self.schema = rule[5]
 
-    def validate(self, socialcalc, doc_id, doc_name, doc_corpus):
-        report = ""
+	def validate(self, socialcalc, doc_id, doc_name, doc_corpus):
+		report = ""
 
-        if self.corpus is not None:
-            if re.search(self.corpus, doc_corpus) is None:
-                return report, False
-        if self.doc is not None:
-            if re.search(self.doc, doc_name) is None:
-                return report, False
+		if self.corpus is not None:
+			if re.search(self.corpus, doc_corpus) is None:
+				return report, False
+		if self.doc is not None:
+			if re.search(self.doc, doc_name) is None:
+				return report, False
 
-        export_data = ether_to_sgml(socialcalc, doc_id, config=self.config)
+		export_data = ether_to_sgml(socialcalc, doc_id, config=self.config)
 
-        schema = self.schema
-        command = "xmllint --schema schemas/" + schema + " tempfilename"
-        out, err = exec_via_temp(export_data, command)
-        err = err.strip()
-        err = err.replace("<br>","").replace("\n","").replace('<h1 align="center">xmllint output</h1>',"")
-        err = re.sub(r'/tmp/[A-Za-z0-9_]+:','XML schema: <br>',err)
-        err = re.sub(r'/tmp/[A-Za-z0-9_]+','XML schema ',err)
-        err = re.sub(r'\n','<br/>',err)
-        if err == "XML schema validates":
-            report = ""
-        else:
-            report = "Problems with exporting with " + self.config \
-                     + " and validating with " + self.schema + ":<br>" + err.decode("utf8") + "<br>"
+		schema = self.schema
+		command = "xmllint --schema schemas/" + schema + " tempfilename"
+		out, err = exec_via_temp(export_data, command)
+		err = err.strip()
+		err = err.replace("<br>","").replace("\n","").replace('<h1 align="center">xmllint output</h1>',"")
+		err = err.replace("<","&lt;").replace(">","&gt;")
+		err = re.sub(r'/tmp/[A-Za-z0-9_]+:','XML schema: <br>',err)
+		err = re.sub(r'/tmp/[A-Za-z0-9_]+','XML schema ',err)
+		err = re.sub(r'\n','<br/>',err)
+		if err.strip() == "XML schema  validates":
+			report = ""
+		else:
+			report = "Problems with exporting with " + self.config \
+					 + " and validating with " + self.schema + ":<br>" + err.decode("utf8") + "<br>"
 
-        return report, True
+		return report, True
