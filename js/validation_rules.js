b5e8d6575210795bc9b80cc9466f4c701b262054
Luke Gessler <lukegessler@gmail.com>
2018-10-30 17:16:35 -0400

    put validation rules tables into tabs

--- js/validation_rules.js
+++ js/validation_rules.js
@@ -221,3 +221,15 @@ $(document).ready(function () {
     });
     $('#export-table-container').jtable('load');
 });
+
+$(document).ready(function(){
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    });
+});
