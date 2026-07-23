7c61ffacc79a9119e88ef5feb871630b4ebe43da
amir-zeldes <amir-zeldes@users.noreply.github.com>
2026-07-23 13:56:53 -0400

    cleanup for v2

--- js/admin.js
+++
@@ -1,7 +0,0 @@
-function export_all(){
-    stylesheet = document.getElementById('ether_stylesheet').value;
-    corpus = document.getElementById('corpus_select').value;
-    extension = document.getElementById('extension_select').value;
-    status = document.getElementById('status_select').value;
-    window.open('export.py?docs=%all%&stylesheet=' + stylesheet + "&corpus=" + corpus + "&extension=" + extension + "&status=" + status, '_new');
-}
