5ce2e426bd458717061aa05663ebfd88d6fce554
amir-zeldes <amir-zeldes@users.noreply.github.com>
2026-08-17 17:23:15 -0400

    sort assignable users in dashboard

--- backend/api.py
+++ backend/api.py
@@ -1044,7 +1044,7 @@ def list_users(project_name: str, current_user: dict = Depends(require_admin(1))
 
     keys = r.keys(f"user:{project_name}:*")
     users = []
-    for key in keys:
+    for key in sorted(keys):
         user_data = r.hgetall(key)
         user_data.pop("password", None)
         user_data.pop("token_session", None)
